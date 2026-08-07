bf80ed5d58bec3c7101123d5f87b00291340d017
Suhani Vyas <suhanii29vyas@gmail.com>
2026-08-07 21:38:08 +0530

    Time: 1 ms (25.59%), Space: 44 MB (61.25%) - LeetHub

--- 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.java
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.java
@@ -1,10 +1,10 @@
 class Solution {
     public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
-        int count=0;
-        for(int i=0;i<hours.length;i++){
-        if(hours[i]>=target){
-        count++;
-        }
+        int count = 0;
+        for (int i = 0; i < hours.length; i++) {
+            if (hours[i] >= target) {
+                count++;
+            }
         }
         return count;
     }
